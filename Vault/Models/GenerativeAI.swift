e99e1db3444c128139ef5ea25d7f139d03753e82
AVesich <75646434+AVesich@users.noreply.github.com>
2024-05-20 10:44:23 -0400

    Updated gitignore and added google ai api

---
+++ Vault/Models/GenerativeAI.swift
@@ -0,0 +1,34 @@
+//
+//  GenerativeAI.swift
+//  Vault
+//
+//  Created by Austin Vesich on 5/20/24.
+//
+
+import Foundation
+import GoogleGenerativeAI
+
+struct GenerativeAI {
+    private var apiKey: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "AI", ofType: "plist") else {
+                fatalError("Failed to find AI.plist")
+            }
+            let plist = NSDictionary(contentsOfFile: filePath)
+            guard let value = plist?.object(forKey: "GoogleKey") as? String else {
+                fatalError("Couldn't find key 'GoogleKey' in 'AI.plist'.")
+            }
+            return value
+        }
+    }
+    private var model: GenerativeModel!
+    
+    init() {
+        model = GenerativeModel(name: "gemini-flash", apiKey: apiKey)
+    }
+    
+    public func getResponse(to prompt: String) async -> String {
+        let response = try? await model.generateContent(prompt)
+        return response?.text ?? "There is no response available at this time."
+    }
+}
