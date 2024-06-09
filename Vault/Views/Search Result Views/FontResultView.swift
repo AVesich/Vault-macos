48b43d769f584ce7dbf3873558669f3b4fc4859d
AVesich <75646434+AVesich@users.noreply.github.com>
2024-06-09 15:07:36 -0600

    Fixed visual resizing bug with font result view

--- Vault/Views/Search Result Views/FontResultView.swift
+++ Vault/Views/Search Result Views/FontResultView.swift
@@ -18,21 +18,25 @@ struct FontResultView: View {
                 Text("Font name")
                     .font(.manrope(12.0, weight: .bold))
                     .padding(.leading, 16.0)
-                Text("The quick brown fox runs over the lazy dog")
+                Text("The quick brown fox runs over the lazy dog.")
                     .font(.manrope(24.0))
+                    .frame(width: .infinity)
             }
-            .frame(width: .infinity)
+
+            Spacer()
+            
             if hovering {
                 Button {
                     // TODO: - Add copy colors on click
                 } label: {
                     Image(systemName: "arrow.down.to.line")
-                        .frame(height: 36.0)
+                        .frame(width: 24.0, height: 24.0)
                 }
                 .buttonStyle(.borderless)
             }
         }
         .animation(.spring(response: 0.25, dampingFraction: 0.55, blendDuration: 1.0))
+        .frame(height: 48.0)
         .onHover { hover in
             hovering = hover
         }
