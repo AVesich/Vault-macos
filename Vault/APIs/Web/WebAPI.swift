e607980631bf85687a569e300bb598aedd277da0
AVesich <75646434+AVesich@users.noreply.github.com>
2024-07-21 18:55:49 -0600

    Added web API

---
+++ Vault/APIs/Web/WebAPI.swift
@@ -0,0 +1,31 @@
+//
+//  WebAPI.swift
+//  Vault
+//
+//  Created by Austin Vesich on 7/21/24.
+//
+
+final class WebAPI: API {
+    
+    // MARK: - Properties
+    internal var apiConfig: APIConfig!
+    internal var results = [any SearchResult]()
+    internal var prevQuery: String?
+    internal var nextPageInfo: NextPageInfo<Int> = NextPageInfo<Int>(nextPageCursor: nil, hasNextPage: false) // Web API will always return 1-2 results. No pagination.
+    internal var isLoadingNewPage: Bool = false
+    private let GOOGLE_QUERY = "https://www.google.com/search?q="
+    
+    // MARK: - Methods
+    public func getResultData(for query: String) async -> APIResponse<Int> {
+        var results = [WebResult]()
+        
+        if let firstURL = query.asOpenableURL() {
+            results.append(WebResult(content: firstURL.absoluteString))
+        }
+        if query.isWebURL() { // Make google query the second result for real websites, otherwise only return query result with code above.
+            results.append(WebResult(content: GOOGLE_QUERY+query))
+        }
+        
+        return APIResponse<Int>(results: results, nextPageInfo: nextPageInfo) // Return the same next page info every time. No pagination should be used.
+    }
+}
---
+++ Vault/Extensions/String+URLutils.swift
@@ -0,0 +1,38 @@
+//
+//  String+URLUtils.swift
+//  Vault
+//
+//  Created by Austin Vesich on 7/21/24.
+//
+
+import Foundation
+
+extension String {
+    public func asOpenableURL() -> URL? {
+        let HTTPS = "https://"
+        let GOOGLE_QUERY = "https://www.google.com/search?q="
+        if !self.stringStartsWithHTTP() && self.isWebURL() {
+            return URL(string: HTTPS+self)
+        }
+        if !self.stringStartsWithHTTP() && !self.isWebURL() {
+            return URL(string: GOOGLE_QUERY+self)
+        }
+        return URL(string: self) // If url starts with http, take the user's word for it and try to search
+    }
+    
+    public func stringStartsWithHTTP() -> Bool {
+        if let HTTP_REGEX = try? Regex("http(s)?"),
+           self.starts(with: HTTP_REGEX) {
+            return true
+        }
+        return false
+    }
+    
+    public func isWebURL() -> Bool {
+        if let URL_REGEX = try? Regex("[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)"),
+           !self.matches(of: URL_REGEX).isEmpty {
+            return true
+        }
+        return false
+    }
+}
